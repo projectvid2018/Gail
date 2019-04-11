1f5cbdceda8ead1ce9772fb25d7b6e9f91594681
projectvid2018 <43933115+projectvid2018@users.noreply.github.com>
2019-04-11 10:30:33 +0600

    Dashboard added

---
+++ app/src/main/java/com/example/gail/Dashboard.java
@@ -0,0 +1,43 @@
+package com.example.gail;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+    CardView search, add, edit, delete, view;
+
+    public void search(android.view.View view) {
+        Intent intent = new Intent(Dashboard.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void add(android.view.View view) {
+        Intent intent = new Intent(Dashboard.this,Add.class);
+        startActivity(intent);
+    }
+
+    public void edit(android.view.View view) {
+        Intent intent = new Intent(Dashboard.this,Edit.class);
+        startActivity(intent);
+    }
+
+    public void delete(android.view.View view) {
+        Intent intent = new Intent(Dashboard.this,Delete.class);
+        startActivity(intent);
+    }
+    public void view(android.view.View view) {
+        Intent intent = new Intent(Dashboard.this, com.example.gail.View.class);
+        startActivity(intent);
+    }
+
+}
