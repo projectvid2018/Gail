4a18c4a49a837da5f5361fa5a8e0936177dbe789
projectvid2018 <43933115+projectvid2018@users.noreply.github.com>
2019-04-14 14:45:21 +0600

    Dashboard changes

--- app/src/main/java/com/example/gail/Dashboard.java
+++ app/src/main/java/com/example/gail/Dashboard.java
@@ -1,6 +1,8 @@
 package com.example.gail;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -14,7 +16,7 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.activity_dashboard);
     }
 
-    CardView search, add, edit, delete, view;
+    CardView search, add, edit, delete, view, exit;
 
     public void search(android.view.View view) {
         Intent intent = new Intent(Dashboard.this,MainActivity.class);
@@ -38,6 +40,34 @@ public class Dashboard extends AppCompatActivity {
     public void view(android.view.View view) {
         Intent intent = new Intent(Dashboard.this, com.example.gail.View.class);
         startActivity(intent);
+    }
+
+    public void exit(android.view.View view) {
+        AlertDialog.Builder builder=new AlertDialog.Builder(Dashboard.this);
+        builder.setMessage("Do you want to log out?");
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+
+                finish();
+                Intent i=new Intent(Dashboard.this,Login.class);
+                i.putExtra("finish", true);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //--------------------------------------- To clean up all activities
+                startActivity(i);
+                finish();
+
+            }
+        });
+
+        builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog alert=builder.create();
+        alert.show();
     }
 
 }
